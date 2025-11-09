0600258aacbb50fafd09b8f196ee0e8873ce79ce
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-11-09 15:34:38 +0100

    Create ClearPlayerDataMethod.cs

---
+++ MethodSystem/Methods/PlayerDataMethods/ClearPlayerDataMethod.cs
@@ -0,0 +1,39 @@
+﻿using SER.ArgumentSystem.Arguments;
+using SER.ArgumentSystem.BaseArguments;
+using SER.MethodSystem.BaseMethods;
+using SER.ValueSystem;
+
+namespace SER.MethodSystem.Methods.PlayerDataMethods;
+
+public class ClearPlayerDataMethod : SynchronousMethod
+{
+    public override string? Description => "Clears data associated with specified players";
+
+    public override Argument[] ExpectedArguments =>
+    [
+        new PlayersArgument("players"),
+        new TextArgument("key to clear")
+        {
+            DefaultValue = new(string.Empty, "all keys"),
+            Description = "Don't provide this argument to clear all keys for players, provide if you want to clear specific key"
+        }
+    ];
+    
+    public override void Execute()
+    {
+        var players = Args.GetPlayers("players");
+        var key = Args.GetText("key to clear");
+        
+        var func = key.IsEmpty()
+            ? (Action<Dictionary<string, Value>>)(dict => dict.Clear()) 
+            : dict => dict.Remove(key);
+        
+        players.ForEach(p =>
+        {
+            if (SetPlayerDataMethod.PlayerData.TryGetValue(p, out var dict))
+            {
+                func(dict);
+            }
+        });
+    }
+}

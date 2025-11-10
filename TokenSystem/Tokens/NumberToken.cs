f9d490fb6027e805fb491bd266858b077edf58c1
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-11-10 16:57:08 +0100

    Update NumberToken.cs

--- TokenSystem/Tokens/NumberToken.cs
+++ TokenSystem/Tokens/NumberToken.cs
@@ -1,5 +1,4 @@
-﻿using SER.Helpers.ResultSystem;
-using SER.ScriptSystem;
+﻿using SER.ScriptSystem;
 using SER.ValueSystem;
 
 namespace SER.TokenSystem.Tokens;
@@ -8,27 +7,18 @@ public class NumberToken : LiteralValueToken<NumberValue>
 {
     protected override IParseResult InternalParse(Script scr)
     {
-        if (TryParse(RawRep).WasSuccessful(out var value))
+        if (decimal.TryParse(RawRep, out var value))
         {
             Value = value;
             return new Success();
         }
 
+        if (RawRep.EndsWith("%") && decimal.TryParse(RawRep.TrimEnd('%'), out value))
+        {
+            Value = value / 100;
+            return new Success();
+        }
+
         return new Ignore();
-    }
-
-    public static TryGet<decimal> TryParse(string stringRep)
-    {
-        if (decimal.TryParse(stringRep, out var value))
-        {
-            return value;
-        }
-
-        if (stringRep.EndsWith("%") && decimal.TryParse(stringRep.TrimEnd('%'), out value))
-        {
-            return value / 100;
-        }
-
-        return $"Value '{stringRep}' is not a valid number";
     }
 }

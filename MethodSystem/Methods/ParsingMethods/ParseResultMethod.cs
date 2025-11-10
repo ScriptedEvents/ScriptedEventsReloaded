91b679077bd1f03cb90b2b5645eb45b03d596716
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-11-10 16:41:30 +0100

    Create ParseResultMethod.cs

---
+++ MethodSystem/Methods/ParsingMethods/ParseResultMethod.cs
@@ -0,0 +1,43 @@
+﻿using SER.ArgumentSystem.Arguments;
+using SER.ArgumentSystem.BaseArguments;
+using SER.Helpers.Exceptions;
+using SER.MethodSystem.BaseMethods;
+using SER.MethodSystem.MethodDescriptors;
+using SER.MethodSystem.Structures;
+using SER.ValueSystem;
+
+namespace SER.MethodSystem.Methods.ParsingMethods;
+
+public class ParseResultMethod : ReturningMethod, ICanError, IReferenceResolvingMethod
+{
+    public override string Description => "Returns information from the parsing result.";
+
+    public override Type[]? ReturnTypes => null;
+
+    public Type ReferenceType => typeof(ParseResult);
+
+    public string[] ErrorReasons =>
+    [
+        "Tried to access the value when the parsing was not successful"
+    ];
+
+    public override Argument[] ExpectedArguments =>
+    [
+        new ReferenceArgument<ParseResult>("parsing result"),
+        new OptionsArgument("info", 
+            new("success", "Returns true if the parsing was successful"), 
+            new("value", "The value that got parsed")
+        )
+    ];
+
+    public override void Execute()
+    {
+        var parseResult = Args.GetReference<ParseResult>("parsing result");
+        ReturnValue = Args.GetOption("info") switch
+        {
+            "success" => new BoolValue(parseResult.Success),
+            "value" => parseResult.Value ?? throw new ScriptRuntimeError(ErrorReasons[0]),
+            _ => throw new AndrzejFuckedUpException()
+        };
+    }
+}

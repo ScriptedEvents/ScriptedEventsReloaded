38621e580614c03ce089c5e48e5e70a3f565062b
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-11-11 11:33:28 +0100

    Create GetFromDBMethod.cs

---
+++ MethodSystem/Methods/DatabaseMethods/GetFromDBMethod.cs
@@ -0,0 +1,35 @@
+﻿using SER.ArgumentSystem.Arguments;
+using SER.ArgumentSystem.BaseArguments;
+using SER.Helpers.Exceptions;
+using SER.MethodSystem.BaseMethods;
+using SER.MethodSystem.MethodDescriptors;
+
+namespace SER.MethodSystem.Methods.DatabaseMethods;
+
+public class GetFromDBMethod : ReturningMethod, ICanError
+{
+    public override string Description => "Returns the value of a given key in the database.";
+
+    public override Type[]? ReturnTypes => null;
+
+    public string[] ErrorReasons =>
+    [
+        "Provided key does not exist in the database."
+    ];
+
+    public override Argument[] ExpectedArguments =>
+    [
+        new DatabaseArgument("database"),
+        new TextArgument("key")
+    ];
+
+    public override void Execute()
+    {
+        if (Args.GetDatabase("database").Get(Args.GetText("key")).HasErrored(out var err, out var value))
+        {
+            throw new ScriptRuntimeError(err);
+        }
+
+        ReturnValue = value;
+    }
+}

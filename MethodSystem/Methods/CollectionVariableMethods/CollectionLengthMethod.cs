bf2f9441f980cff5a5e254ae8d2932d8d1a58469
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-11-11 16:12:28 +0100

    Create CollectionLengthMethod.cs

---
+++ MethodSystem/Methods/CollectionVariableMethods/CollectionLengthMethod.cs
@@ -0,0 +1,21 @@
+﻿using SER.ArgumentSystem.Arguments;
+using SER.ArgumentSystem.BaseArguments;
+using SER.MethodSystem.BaseMethods;
+using SER.ValueSystem;
+
+namespace SER.MethodSystem.Methods.CollectionVariableMethods;
+
+public class CollectionLengthMethod : ReturningMethod<NumberValue>
+{
+    public override string? Description => "Returns the amount of items in a collection.";
+
+    public override Argument[] ExpectedArguments =>
+    [
+        new CollectionArgument("collection")
+    ];
+    
+    public override void Execute()
+    {
+        ReturnValue = Args.GetCollection("collection").CastedValues.Length;
+    }
+}

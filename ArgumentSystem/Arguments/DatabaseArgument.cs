b71b0b99ec5bef1ae335d8009bb5e754f6ac4e97
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-11-11 11:33:22 +0100

    Create DatabaseArgument.cs

---
+++ ArgumentSystem/Arguments/DatabaseArgument.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using SER.ArgumentSystem.BaseArguments;
+using SER.FileSystem.Structures;
+using SER.Helpers.ResultSystem;
+using SER.TokenSystem.Tokens;
+
+namespace SER.ArgumentSystem.Arguments;
+
+public class DatabaseArgument(string name) : Argument(name)
+{
+    public override string InputDescription => "Database name";
+    
+    [UsedImplicitly]
+    public DynamicTryGet<Database> GetConvertSolution(BaseToken token)
+    {
+        return new(() => Database.TryGet(token.GetBestTextRepresentation(Script)));
+    }
+}

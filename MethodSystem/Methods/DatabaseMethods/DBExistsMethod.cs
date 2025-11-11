d63560ff9a1ed5ad7b1b578d8ac790f994bb761a
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-11-11 16:05:04 +0100

    Create DBExistsMethod.cs

---
+++ MethodSystem/Methods/DatabaseMethods/DBExistsMethod.cs
@@ -0,0 +1,22 @@
+﻿using SER.ArgumentSystem.Arguments;
+using SER.ArgumentSystem.BaseArguments;
+using SER.FileSystem.Structures;
+using SER.MethodSystem.BaseMethods;
+using SER.ValueSystem;
+
+namespace SER.MethodSystem.Methods.DatabaseMethods;
+
+public class DBExistsMethod : ReturningMethod<BoolValue>
+{
+    public override string? Description => "Returns true if the provided database exists.";
+
+    public override Argument[] ExpectedArguments =>
+    [
+        new TextArgument("database name")
+    ];
+    
+    public override void Execute()
+    {
+        ReturnValue = Database.TryGet(Args.GetText("database name")).WasSuccessful();
+    }
+}

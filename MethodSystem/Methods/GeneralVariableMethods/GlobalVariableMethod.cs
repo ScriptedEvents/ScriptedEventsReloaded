8b6c49fb84df1df8ca10a58aa56b542cf0958189
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2025-11-22 17:07:40 +0100

    add IError to GlobalVariable method

--- MethodSystem/Methods/GeneralVariableMethods/GlobalVariableMethod.cs
+++ MethodSystem/Methods/GeneralVariableMethods/GlobalVariableMethod.cs
@@ -2,20 +2,26 @@
 using SER.ArgumentSystem.BaseArguments;
 using SER.Helpers.Exceptions;
 using SER.MethodSystem.BaseMethods;
+using SER.MethodSystem.MethodDescriptors;
 using SER.TokenSystem.Tokens.VariableTokens;
 using SER.VariableSystem;
 
 namespace SER.MethodSystem.Methods.GeneralVariableMethods;
 
-public class GlobalVariableMethod : SynchronousMethod
+public class GlobalVariableMethod : SynchronousMethod, ICanError
 {
     public override string Description => "Makes a provided local variable into a global variable.";
+
+    public string[] ErrorReasons =>
+    [
+        "Provided variable does not exist."
+    ];
 
     public override Argument[] ExpectedArguments { get; } =
     [
         new TokenArgument<VariableToken>("variable to make global")
     ];
-    
+
     public override void Execute()
     {
         var variableToken = Args.GetToken<VariableToken>("variable to make global");

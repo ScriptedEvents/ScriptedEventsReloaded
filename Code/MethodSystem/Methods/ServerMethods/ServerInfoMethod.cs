e8875e86614bcf0a218347554549e773620630e1
Tosoks67 <tosoks67@gmail.com>
2026-01-24 12:01:05 +0100

    ReplaceTextInVariable, ServerInfo methods

---
+++ Code/MethodSystem/Methods/ServerMethods/ServerInfoMethod.cs
@@ -0,0 +1,46 @@
+﻿using JetBrains.Annotations;
+using LabApi.Features.Wrappers;
+using SER.Code.ArgumentSystem.Arguments;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.Helpers.Exceptions;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+using SER.Code.ValueSystem;
+
+namespace SER.Code.MethodSystem.Methods.ServerMethods;
+
+[UsedImplicitly]
+public class ServerInfoMethod : ReturningMethod
+{
+    public override string Description => "Returns info about the server.";
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new OptionsArgument("info",
+            "ip",
+            "port",
+            "name",
+            "maxPlayers",
+            "tps",
+            "isVerified")
+    ];
+
+    public override TypeOfValue Returns => new TypesOfValue([
+        typeof(TextValue),
+        typeof(NumberValue),
+        typeof(BoolValue)
+    ]);
+
+    public override void Execute()
+    {
+        ReturnValue = Args.GetOption("info") switch
+        {
+            "ip" => new TextValue(Server.IpAddress),
+            "port" => new NumberValue(Server.Port),
+            "name" => new TextValue(Server.ServerListName),
+            "maxplayers" => new NumberValue(Server.MaxPlayers),
+            "tps" => new NumberValue((decimal)Server.Tps),
+            "isverified" => new BoolValue(CustomNetworkManager.IsVerified),
+            _ => throw new TosoksFuckedUpException("out of order")
+        };
+    }
+}
---
+++ Code/MethodSystem/Methods/TextMethods/ReplaceTextInVariableMethod.cs
@@ -0,0 +1,44 @@
+﻿using JetBrains.Annotations;
+using SER.Code.ArgumentSystem.Arguments;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.Helpers.Exceptions;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+using SER.Code.MethodSystem.MethodDescriptors;
+using SER.Code.ValueSystem;
+using SER.Code.VariableSystem.Variables;
+
+namespace SER.Code.MethodSystem.Methods.TextMethods;
+
+[UsedImplicitly]
+public class ReplaceTextInVariableMethod : SynchronousMethod, ICanError
+{
+    public override string Description => "Replaces given values in a given text variable.";
+
+    public string[] ErrorReasons =>
+    [
+        "The given variable is not a text variable."
+    ];
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new LiteralVariableArgument("text variable to perform the replacement on"),
+        new TextArgument("text to replace"),
+        new TextArgument("replacement text")
+    ];
+    
+    public override void Execute()
+    {
+        var variable = Args.GetLiteralVariable("text variable to perform the replacement on");
+        var text = Args.GetText("text to replace");
+        var replacement = Args.GetText("replacement text");
+
+        if ((LiteralValue)variable is not TextValue textValue)
+            throw new ScriptRuntimeError(this, ErrorReasons[0]);
+        
+        Script.AddVariable(
+            new LiteralVariable(
+                variable.Name,
+                new TextValue(textValue.Value.Replace(text, replacement))));
+
+    }
+}

9857b31238ccf18050766811dc2d37c0c7d026de
Tosoks67 <tosoks67@gmail.com>
2026-01-26 17:43:30 +0100

    new EditDiscordMessage method

---
+++ Code/MethodSystem/Methods/DiscordMethods/EditDiscordMessageMethod.cs
@@ -0,0 +1,47 @@
+﻿using JetBrains.Annotations;
+using MEC;
+using SER.Code.ArgumentSystem.Arguments;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.Helpers.Exceptions;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+using SER.Code.MethodSystem.MethodDescriptors;
+using SER.Code.MethodSystem.Methods.HTTPMethods;
+
+namespace SER.Code.MethodSystem.Methods.DiscordMethods;
+
+[UsedImplicitly]
+public class EditDiscordMessageMethod : SynchronousMethod, ICanError
+{
+    public override string Description => "Edits a message sent by a discord webhook (with that same webhook).";
+
+    public string[] ErrorReasons =>
+    [
+        ..HTTPPostMethod.HttpErrorReasons,
+        "Message ID must not be empty.",
+        "Provided URL is not a valid webhook URL."
+    ];
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new TextArgument("webhook url"),
+        new TextArgument("message id"),
+        new ReferenceArgument<DiscordMessageMethod.DMessage>("message object")
+    ];
+
+    public override void Execute()
+    {
+        var webhookUrl = Args.GetText("webhook url");
+        var messageId = Args.GetText("message id");
+        var messageObject = Args.GetReference<DiscordMessageMethod.DMessage>("message object");
+        
+        if (messageId.IsEmpty())
+            throw new ScriptRuntimeError(this, ErrorReasons[^2]);
+        
+        if (!webhookUrl.StartsWith("https://discord.com/api/webhooks/"))
+            throw new ScriptRuntimeError(this, ErrorReasons.Last());
+        
+        var messageURL = webhookUrl + "/messages/" + messageId;
+
+        Timing.RunCoroutine(HTTPPostMethod.RequestSend(this, messageURL, messageObject, "PATCH"));
+    }
+}

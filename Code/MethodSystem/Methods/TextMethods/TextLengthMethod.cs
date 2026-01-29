2323451bd355600f022fdf99ff286c2ef951fc52
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-01-29 12:59:44 +0100

    Create TextLengthMethod.cs

---
+++ Code/MethodSystem/Methods/TextMethods/TextLengthMethod.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using SER.Code.ArgumentSystem.Arguments;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+using SER.Code.ValueSystem;
+
+namespace SER.Code.MethodSystem.Methods.TextMethods;
+
+[UsedImplicitly]
+public class TextLengthMethod : ReturningMethod<NumberValue>
+{
+    public override string Description => "Returns the length of the text.";
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new TextArgument("text")
+    ];
+    
+    public override void Execute()
+    {
+        ReturnValue = Args.GetText("text").Length;
+    }
+}

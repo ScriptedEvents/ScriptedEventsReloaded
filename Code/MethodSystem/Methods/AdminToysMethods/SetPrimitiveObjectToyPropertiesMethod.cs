3f630de9c4fef4d0634bbdb1fc1b76d1e0226317
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-02-03 15:53:04 +0100

    Create SetPrimitiveObjectToyPropertiesMethod.cs

---
+++ Code/MethodSystem/Methods/AdminToysMethods/SetPrimitiveObjectToyPropertiesMethod.cs
@@ -0,0 +1,41 @@
+﻿using AdminToys;
+using JetBrains.Annotations;
+using SER.Code.ArgumentSystem.Arguments;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+using UnityEngine;
+using PrimitiveObjectToy = LabApi.Features.Wrappers.PrimitiveObjectToy;
+
+namespace SER.Code.MethodSystem.Methods.AdminToysMethods;
+
+[UsedImplicitly]
+public class SetPrimitiveObjectToyPropertiesMethod : SynchronousMethod
+{
+    public override string Description => "Sets properties of a primitive object toy.";
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new ReferenceArgument<PrimitiveObjectToy>("toy reference"),
+        new EnumArgument<PrimitiveType>("type")
+        {
+            DefaultValue = new(null, "not changing")
+        },
+        new ColorArgument("color")
+        {
+            DefaultValue = new(null, "not changing")
+        },
+        new FlagsArgument<PrimitiveFlags>("flags")
+        {
+            DefaultValue = new(null, "not changing")
+        },
+    ];
+    
+    public override void Execute()
+    {
+        var toy = Args.GetReference<PrimitiveObjectToy>("toy reference");
+
+        if (Args.GetNullableEnum<PrimitiveType>("type") is { } type) toy.Type = type;
+        if (Args.GetNullableColor("color") is { } color) toy.Color = color;
+        if (Args.GetNullableFlags<PrimitiveFlags>("flags") is { } flags) toy.Flags = flags;
+    }
+}

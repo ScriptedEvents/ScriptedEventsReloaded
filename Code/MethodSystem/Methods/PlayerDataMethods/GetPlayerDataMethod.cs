303ecd09c1b9db2e290c5a37ffb724ba1d9ae182
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-02-24 18:04:30 +0100

    Update GetPlayerDataMethod.cs

--- Code/MethodSystem/Methods/PlayerDataMethods/GetPlayerDataMethod.cs
+++ Code/MethodSystem/Methods/PlayerDataMethods/GetPlayerDataMethod.cs
@@ -2,18 +2,29 @@
 using SER.Code.ArgumentSystem.Arguments;
 using SER.Code.ArgumentSystem.BaseArguments;
 using SER.Code.Exceptions;
+using SER.Code.MethodSystem.BaseMethods;
 using SER.Code.MethodSystem.BaseMethods.Synchronous;
+using SER.Code.MethodSystem.MethodDescriptors;
 using SER.Code.ValueSystem;
 
 namespace SER.Code.MethodSystem.Methods.PlayerDataMethods;
 
 [UsedImplicitly]
-public class GetPlayerDataMethod : ReturningMethod
+public class GetPlayerDataMethod : ReturningMethod, IAdditionalDescription, ICanError
 {
     public override string Description => "Gets player data from the key.";
 
-    public override TypeOfValue Returns => new UnknownTypeOfValue();
+    public string AdditionalDescription => 
+        "WARNING: This method will error if the key doesn't exist. " +
+        $"Use {GetFriendlyName(typeof(HasPlayerDataMethod))} to verify if a key exists before calling this method.";
     
+    public override TypeOfValue Returns => new UnknownTypeOfValue();
+
+    public string[] ErrorReasons { get; } =
+    [
+        "Key was not found for the player."
+    ];
+
     public override Argument[] ExpectedArguments { get; } =
     [
         new PlayerArgument("player"),
@@ -28,7 +39,7 @@ public class GetPlayerDataMethod : ReturningMethod
         if (!SetPlayerDataMethod.PlayerData.TryGetValue(player, out var dict) || 
             !dict.TryGetValue(key, out var value))
         {
-            throw new ScriptRuntimeError(this, $"Key '{key}' was not found for player '{player.Nickname}'.");
+            throw new ScriptRuntimeError(this, ErrorReasons[0]);
         }
 
         ReturnValue = value;

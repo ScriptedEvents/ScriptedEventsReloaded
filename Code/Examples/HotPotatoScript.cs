a58b61a0f515e3042a3d03f4c299736c7f0e30b9
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-03-08 13:34:26 +0100

    Update HotPotatoScript.cs

--- Code/Examples/HotPotatoScript.cs
+++ Code/Examples/HotPotatoScript.cs
@@ -1,5 +1,8 @@
-﻿namespace SER.Code.Examples;
+﻿using JetBrains.Annotations;
 
+namespace SER.Code.Examples;
+
+[UsedImplicitly]
 public class HotPotatoScript : Example
 {
     public override string Name => "hotPotato";
@@ -14,6 +17,11 @@ public class HotPotatoScript : Example
             # Get a random player from the alive players
             @potatoCarrier = LimitPlayers @alivePlayers 1
 
+            # if no player is alive, continue to next attempt
+            if {AmountOf @potatoCarrier} is 0
+                continue
+            end
+            
             Hint @potatoCarrier 3s "YOU HAVE THE HOT POTATO! DROP IT OR DIE!"
             GiveItem @potatoCarrier GunA7
 
@@ -27,6 +35,7 @@ public class HotPotatoScript : Example
                     continue
                 end
 
+                AdvDestroyItem *item
                 Explode @potatoCarrier
                 Broadcast @all 5s "{@potatoCarrier name} failed the Hot Potato!"
                 stop

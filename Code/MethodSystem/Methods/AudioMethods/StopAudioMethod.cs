fbe9fccfb1aec98e831b88b7effcabd6c80f652b
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-04-30 11:55:48 +0200

    Create StopAudioMethod.cs

---
+++ Code/MethodSystem/Methods/AudioMethods/StopAudioMethod.cs
@@ -0,0 +1,33 @@
+using SER.Code.ArgumentSystem.Arguments;
+using SER.Code.ArgumentSystem.BaseArguments;
+using SER.Code.Exceptions;
+using SER.Code.MethodSystem.BaseMethods.Synchronous;
+using SER.Code.MethodSystem.MethodDescriptors;
+
+namespace SER.Code.MethodSystem.Methods.AudioMethods;
+
+[UsedImplicitly]
+public class StopAudioMethod : SynchronousMethod, ICanError
+{
+    public override string Description => "Plays a loaded audio clip from a created speaker.";
+
+    public string[] ErrorReasons =>
+    [
+        "Speaker does not exist."
+    ];
+
+    public override Argument[] ExpectedArguments { get; } =
+    [
+        new TextArgument("speaker name")
+    ];
+
+    public override void Execute()
+    {
+        var speakerName = Args.GetText("speaker name");
+        
+        if (!AudioPlayer.TryGet(speakerName, out AudioPlayer audioPlayer))
+            throw new ScriptRuntimeError(this, $"Speaker with name '{speakerName}' does not exist.");
+        
+        audioPlayer.RemoveAllClips();
+    }
+}

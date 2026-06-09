7ed59e30706620bc08d8d651b285840e67aa3b08
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-06-09 13:58:42 +0200

    fix framework methods being registered multiple times

--- Code/MethodSystem/MethodIndex.cs
+++ Code/MethodSystem/MethodIndex.cs
@@ -25,7 +25,7 @@ public static class MethodIndex
     internal static void Initialize()
     {
         NameToMethodIndex.Clear();
-        
+        FrameworkDependentMethods.Clear();
         AddAllDefinedMethodsInAssembly();
     }
     

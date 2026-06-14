756fadc59c30fa1bb5249f3b915f797328b05d56
Andrzej <100864896+Elektryk-Andrzej@users.noreply.github.com>
2026-06-14 11:51:39 +0200

    Update IsAllowedMethod.cs

--- Code/MethodSystem/Methods/EventMethods/IsAllowedMethod.cs
+++ Code/MethodSystem/Methods/EventMethods/IsAllowedMethod.cs
@@ -11,8 +11,8 @@ public class IsAllowedMethod : SynchronousMethod, IAdditionalDescription
     public override string Description => "Sets whether or not the event is allowed to run.";
 
     public string AdditionalDescription =>
-        "In order for it to have any impact, the script in which the method is used must be trigger by an event, and " +
-        "that event must be cancellable.";
+        "In order for it to have any impact, the script in which the method is used must be triggered by an event, " +
+        "and that event must be cancellable.";
 
     public override Argument[] ExpectedArguments { get; } =
     [
